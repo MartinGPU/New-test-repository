c007fa81db80a439c68da879d0041991fb477214
Marat <maratussr@gmail.com>
2023-01-12 20:03:49 +0400

    add first test-second try

--- src/test/java/com/marat/tests/GitTest.java
+++ src/test/java/com/marat/tests/GitTest.java
@@ -1,2 +1,24 @@
-package com.marat.tests;public class GitTest {
+package com.marat.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.sleep;
+
+public class GitTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        Configuration.browserSize = "1366x768";
+    }
+
+    @Test
+    public void testForGit() {
+        Selenide.open("https://www.avito.ru/");
+        $(".top-rubricator-rubricatorButton-SoKyQ").click();
+        sleep(4000);
+    }
 }
